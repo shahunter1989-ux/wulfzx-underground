26c20ed2a6dca62dc0753250349aaea0e1c8859d
Wulfzx.Underground <shahunter1989@gmail.com>
2026-06-21 23:21:56 -0700

    Add static route for What I Do page

---
+++ scripts/prepare-static-routes.mjs
@@ -0,0 +1,13 @@
+import { copyFile, mkdir } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const distDir = 'dist'
+const routes = ['what-i-do']
+
+await Promise.all(
+  routes.map(async (route) => {
+    const routeDir = join(distDir, route)
+    await mkdir(routeDir, { recursive: true })
+    await copyFile(join(distDir, 'index.html'), join(routeDir, 'index.html'))
+  }),
+)
